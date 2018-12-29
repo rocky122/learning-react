95cc0ecb3f48742598d21931e5a3a843ac61a8a4
rocky122 <38912058+rocky122@users.noreply.github.com>
2018-12-29 16:53:22 +0530

    lifecycles done (fuu)

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -13,20 +13,38 @@ class LifecycleA extends Component {
     console.log("Lifecycle A getDerivedStateFromProps called");
     return null;
   }
-  // render is the third lifecycle hook for the mounting phase
-  render() {
-    console.log("Lifecycle A render method called!");
-    return (
-      <div>
-        <h1>Lifecycle A render method called</h1>
-        <LifecycleB />
-      </div>
-    );
+  changeState = () => {
+    this.setState({ name: 'Prateek' });
   }
   //cdm method is the final method for the mounting phase
   //   to cause side effects it is the best place to do so
   componentDidMount() {
     console.log("Lifecycle A componentDidMount called....");
+  }
+  //update lc hook 
+  shouldComponentUpdate(){
+    console.log("LCA shouldComponentUpdate called.....");
+    return true;
+  }
+  componentDidUpdate() {
+    console.log('componentDidUpdate called....');
+  }
+  //update lc hook
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('LCA getSnapshotBeforeUpdate called.....');
+    return null;
+  }
+  
+  // render is the third lifecycle hook for the mounting phase
+  render() {
+    console.log("Lifecycle A render method called!");
+    return(
+      <div>
+        <h1>Lifecycle A render method called</h1>
+        <button onClick={this.changeState}>Change State</button>
+        <LifecycleB />
+      </div>
+    )
   }
 }
 
--- src/components/LifecycleB.js
+++ src/components/LifecycleB.js
@@ -1,31 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 class LifecycleB extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    console.log("[LCB]Inside the constructor method");
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log("[LCB] getDerivedStateFromProps called....");
+    return null;
+  }
 
-   constructor(props) {
-     super(props)
-     this.state = {
-        
-     }
-     console.log('[LCB]Inside the constructor method');
-   }
-   static getDerivedStateFromProps(props,state) {
-       console.log('[LCB] getDerivedStateFromProps called....');
-       return null;
-   }
-   
-    componentDidMount = () => {
-      console.log('[LCB] componentDidMount called....')
-    }
-    
+  componentDidMount = () => {
+    console.log("[LCB] componentDidMount called....");
+  };
+  //update lc hook
+  shouldComponentUpdate() {
+    console.log("LCB shouldComponentUpdate called.....");
+    return true;
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate called....");
+  }
+  //update lc hook
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("LCB getSnapshotBeforeUpdate called.....");
+    return null;
+  }
+
   render() {
-    console.log('[LCB] render method called....')
+    console.log("[LCB] render method called....");
     return (
       <div>
-        <h1>Hey render was invoked and I was rendered as 
-        a result of it...</h1>
+        <h1>Hey render was invoked and I was rendered as a result of it...</h1>
       </div>
-    )
+    );
   }
 }
 
-export default LifecycleB
+export default LifecycleB;
